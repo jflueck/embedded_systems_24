0b61516b4b5ecacf5dc7440c0791a6f0c27d520e
Robert Jomar Malate <robjmal@gmail.com>
2024-09-09 16:56:00 +0200

    Finished section 1. Pushing changes for section 2

--- work/ecs/src/lab1/lab1.c
+++ work/ecs/src/lab1/lab1.c
@@ -1,33 +1,154 @@
-#include "eecs461.h"
+/**************************************************************************
+ * FILE NAME: lab1.c
+ *
+ * University of Michigan
+ * EECS 461, Embedded Control Systems
+ * Lab 1
+ * Read Chapter 11&12 in S32K144 User's Manual
+ *
+ * Revision History:
+ *  2014-03-17  Yitian Chen
+ *  2014-07-14  GPCz
+ *  2014-07-19  GPCz
+ *  2017-03     RMC
+ *  2018-03     Paul Domanico
+ *  2018-11-15  jfr
+ *
+ * Revised and adapted to use at IDSC ETH Zurich
+ * 151-0593-00 Embedded Control Systems (ECS)
+ ***************************************************************************/
+
+#include "ecs.h"
 #include <stdint.h>
 
-#define SECTION 1
-#if SECTION == 1
 #include "lab1.h"
+#include "gpio.h"
 
-void main() {
-    volatile uint32_t * const portD_PCR = 0x44; 
-    volatile uint32_t * const portE_PCR = (uint32_t *)(BASE_PORT_E);
-    GPIO_mem * const gpioD = (GPIO_mem *)(BASE_GPIO + GPIOD_OFFSET);
-    GPIO_mem * const gpioE = (GPIO_mem *)(BASE_GPIO + GPIOE_OFFSET);
+/* Change section number to match the section (1-3) being tested */
+#define SECTION 1
 
-    uint16_t sum, value1, value2;
-    uint32_t regReadVal;
-    int index;
+#if SECTION == 1
 
-    /* Configure the LED pins */
-    for (index = 0; index < 5; index++) {
-        /* Configure pin mux to gpio */
-        portD_PCR[index] = 0b001 << 8;
-        /* Set the direction to output */
-        gpioD->PDDR |= 0b1 << index;
-    }
 
-    /* Configure the DIP pins */
-    for (index = 6; index < 14; index++) {
-        /* Configure pin mux to gpio */
-        portE_PCR[index] = 0b001 << 8;
-        /* Set the direction to input */
-        gpioE->PDDR &= ~(1 << index);
-    }
+
+/******************************************************************************
+ * Function:    section1
+ * Description: read and write to the GPIO registers using your header file
+ ******************************************************************************/
+void section1(){
+  /* code for section 1 */
+  volatile uint32_t * const portD_PCR = (uint32_t *)(BASE_PORT_D);
+  volatile uint32_t * const portE_PCR = (uint32_t *)(BASE_PORT_E);
+  GPIO_mem * const gpioD = (GPIO_mem *)(BASE_GPIO + GPIOD_OFFSET);
+  GPIO_mem * const gpioE = (GPIO_mem *)(BASE_GPIO + GPIOE_OFFSET);
+
+  uint16_t sum, value1, value2;
+  uint32_t regReadVal;
+  int index;
+
+  /* Configure the LED pins */
+  for (index = 0; index < 5; index++) {
+      /* Configure pin mux to gpio */
+      portD_PCR[index] = 0b001 << 8;
+      /* Set the direction to output */
+      gpioD->PDDR |= 0b1 << index;
+  }
+
+  /* Configure the DIP pins */
+  for (index = 6; index < 14; index++) {
+      /* Configure pin mux to gpio */
+      portE_PCR[index] = 0b001 << 8;
+      /* Set the direction to input */
+      gpioE->PDDR &= ~(1 << index);
+  }
+
+  /* Section 4.1.1 */
+  gpioD ->PDOR = 0;
+  while (1) {
+    value1 = (gpioE->PDIR >> 6) & 0b1111;
+    value2 = (gpioE->PDIR >> 10) & 0b1111;
+    sum = value1 + value2;
+    gpioD->PDOR = sum;
+  }
+}
+
+
+#elif SECTION == 2
+#include "S32K144.h"
+#include "gpio.h"
+/******************************************************************************
+ * Function:    section2
+ * Description: Use the provided S32K144.h header file
+ ******************************************************************************/
+void section2(){
+  /* code for section 2 */
+  /* Use the Arrays LED, LED_BASE, DIP and DIP_BASE provided in the gpio.c file */
+  const uint8_t LED[16] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+  const uint8_t LED_BASE[16] = {3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3};
+
+  const uint8_t DIP[16] = {6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 0, 1, 2, 3, 17};
+  const uint8_t DIP_BASE[16] = {4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 3};
+
+  int led_array_length = sizeof(LED) / sizeof(LED[0]);
+  int dip_array_length = sizeof(DIP) / sizeof(DIP[0]);
+
+  // Initializes the inputs
+  for (int index=0, index < dip_array_length + 1, index++){
+    initGDPI(DIP_BASE[index], DIP[index]);
+  }
+
+  // Initializes the outputs
+  for (int index=0, index < led_array_length + 1, index++){
+    initGDPO(LED_BASE[index], LED[index]);
+  }
+
+  // Performs calculations and outputs it to LEDs
+
+  uint16_t sum, value1, value2;
+  while (1) {
+    value1 = readGPIO(DIP_BASE, DIP[0])
+    value2 = readGPIO(DIP_BASE, DIP[5])
+    sum = value1 + value2
+    writeGPIO(LED_BASE, LED[0], sum);
+  }
+}
+
+
+#elif SECTION == 3
+#include "LPUART.h"
+#include "gpio.h"
+/******************************************************************************
+ * Function:    section3
+ * Description: Communicate with the computer using the keyboard interface
+ ******************************************************************************/
+void section3(){
+  /* code for section 3 */
+  char byte_in, old_byte_in;
+
+  /* LPUART functions for sending and receiving serial data */
+  LPUART1_init();
+  LPUART1_transmit_string("\n\rSerial Output Enable...........");
+  while(1) {
+    old_byte_in = byte_in;
+    byte_in = LPUART1_receive_char(); // returns ASCII representation
+
+
+  }
+}
+#endif
+
+int main(){
+  /* Always call initECS() before doing anything else */
+  initECS();
+
+  #if(SECTION == 1)
+  section1();
+  #elif(SECTION == 2)
+  section2();
+  #elif(SECTION == 3)
+  section3();
+  #endif
+
+  /* Should never reach this point */
+  return 0;
 }
---
+++ work/ecs/src/lib/gpio.c
@@ -0,0 +1,88 @@
+/**************************************************************************
+ * FILE NAME: gpio.c
+ *
+ * University of Michigan
+ * EECS 461, Embedded Control Systems
+ * General-Purpose Input/Output module
+ * Read Chapter 12&13 in S32K144 User's Manual
+ *
+ * Revision History
+ *  2014-02-08  Yitian Chen
+ *  2014-07-04  GPCz
+ *  2017-03-14  RMC
+ *  2018-03-30  Paul Domanico
+ *
+ * Revised and adapted to use at IDSC ETH Zurich
+ * 151-0593-00 Embedded Control Systems (ECS)
+ **************************************************************************/
+
+#include "gpio.h"
+
+const uint8_t LED[16] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+const uint8_t LED_BASE[16] = {3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3};
+const uint8_t DIP[16] = {6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 0, 1, 2, 3, 17};
+const uint8_t DIP_BASE[16] = {4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 3};
+
+/* base: 0-4 corresponding to gpio/port groups A-E */
+static GPIO_Type * gpio_base_ptrs[5] = GPIO_BASE_PTRS;
+static PORT_Type * port_base_ptrs[5] = PORT_BASE_PTRS;
+
+
+/******************************************************************************
+ * Function:    initGPDI
+ * Description: Initialize a general purpose input based on port base and pin
+ ******************************************************************************/
+void initGPDI(const uint8_t base, const uint8_t pin){
+  /* Set the pin direction to input */
+  gpio_base_ptrs[base]->PDDR &= ~(1 << pin);
+
+  /* Pin Mux Alt1 for GPIO (Setting MUX to GPIO) */
+  port_base_ptrs[base]->PCR[pin] = 0b001 << 8;
+
+  /* All other bitfields to 0 */
+  // gpio_base_ptrs[base]->PDDR &= 0x0000;
+
+  /* Internal pulldown or input filter are optional */
+
+}
+
+
+/******************************************************************************
+ * Function:    initGPDO
+ * Description: Initialize a general purpose output based on port base and pin
+ ******************************************************************************/
+void initGPDO(const uint8_t base, const uint8_t pin) {
+  /* Pin Mux Alt1 for GPIO */
+  port_base_ptrs[base]->PCR[pin] = 0b001 << 8;
+
+  /* Set the pin direction to output */
+  gpio_base_ptrs[base]->PDDR |= 0b1 << index;
+
+  /* All other bitfields to 0 */
+  gpio_base_ptrs[base]->PDOR = 0;
+}
+
+
+/******************************************************************************
+ * Function:    readGPIO
+ * Description: Read the digital 1/0 from GPIO input register
+ ******************************************************************************/
+uint8_t readGPIO(const uint8_t base, const uint8_t pin){
+  return (gpio_base_ptrs[base]->PDIR) >> pin & 0b1111;
+}
+
+
+/******************************************************************************
+ * Function:    writeGPIO
+ * Description: Write a digital 1/0 to a GPIO output
+ ******************************************************************************/
+void writeGPIO(const uint8_t base, const uint8_t pin, const uint8_t val){
+  /*  make sure that
+ (1) your function can be called multiple times in succession to correctly set
+     multiple bits in the PDOR register
+ (2) if a PDOR bit already has the desired setting,
+     then do not toggle the bit before returning it to this setting */
+  gpio_base_ptrs[base]->PDOR = val
+
+  return;
+}
